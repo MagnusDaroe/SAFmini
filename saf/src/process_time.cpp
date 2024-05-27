ce6df1b56d82c3727cae90fb554ba9895ae59e9b
MagnusDaroe <116544128+MagnusDaroe@users.noreply.github.com>
2024-05-27 11:33:53 +0200

    Update process_time.cpp

--- saf/src/process_time.cpp
+++ saf/src/process_time.cpp
@@ -1,8 +1,11 @@
 #include "rclcpp/rclcpp.hpp"
+
+# Here a processtime node could be implemented in Cpp
+
 int main(int argc, char **argv)
 {
     rclcpp::init(argc, argv);
-    auto node = std::make_shared<rclcpp::Node>("drone_control_node");
+    auto node = std::make_shared<rclcpp::Node>("process_time");
     rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
